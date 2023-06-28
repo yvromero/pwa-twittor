e04c3e0b98cee690b02a47452455fe5f0f3d1bc5
yvromero <yvonne.romero@uds.edu.py>
2023-06-28 13:53:54 -0400

    Update para ios versiones iOS +X

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 var url = window.location.href;
 var swLocation = '/pwa-twittor/sw.js';
 
+
 if ( navigator.serviceWorker ) {
+
 
     if ( url.includes('localhost') ) {
         swLocation = '/sw.js';
-
     }
+
+
     navigator.serviceWorker.register( swLocation );
 }
-
 
 
 // Referencias de jQuery
--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
 // imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
+
+const STATIC_CACHE    = 'static-v4';
+const DYNAMIC_CACHE   = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
+
 
 const APP_SHELL = [
     // '/',
@@ -26,17 +28,23 @@ const APP_SHELL_INMUTABLE = [
     'css/animate.css',
     'js/libs/jquery.js'
 ];
+
+
+
 self.addEventListener('install', e => {
 
-    const cacheStatic = caches.open( STATIC_CACHE ).then(cache =>
+    const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
         cache.addAll( APP_SHELL ));
 
-    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache =>
+    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
         cache.addAll( APP_SHELL_INMUTABLE ));
 
-    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
+    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ]) );
 
 });
+
+
+
 
 
 self.addEventListener('activate', e => {
@@ -62,7 +70,10 @@ self.addEventListener('activate', e => {
 });
 
 
+
+
 self.addEventListener( 'fetch', e => {
+
 
     const respuesta = caches.match( e.request ).then( res => {
 
@@ -75,9 +86,14 @@ self.addEventListener( 'fetch', e => {
                 return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
 
             });
+
         }
 
     });
 
+
+
     e.respondWith( respuesta );
+
 });
+
