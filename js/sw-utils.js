d9498c1dcd0c56ba6d067ec54ff9b878eeab9145
yvromero <yvonne.romero@uds.edu.py>
2023-06-28 09:11:53 -0400

    Add recursos para la pwa

---
+++ js/sw-utils.js
@@ -0,0 +1,17 @@
+// Guardar en el cache dinamico
+function actualizaCacheDinamico( dynamicCache, req, res ) {
+
+    if ( res.ok ) {
+
+        return caches.open( dynamicCache ).then( cache => {
+
+            cache.put( req, res.clone() );
+
+            return res.clone();
+        }); 
+
+    } else {
+        return res;
+    }
+
+}
--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const STATIC_CACHE = 'static-v1';
+// imports
+importScripts('js/sw-utils.js');
+
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -12,7 +15,8 @@ const APP_SHELL = [
     'img/avatars/spiderman.jpg',
     'img/avatars/thor.jpg',
     'img/avatars/wolverine.jpg',
-    'js/app.js'
+    'js/app.js',
+    'js/sw-utils.js'
 
 ];
 
@@ -52,8 +56,29 @@ self.addEventListener('activate', e => {
 
     });
 
-
-
     e.waitUntil( respuesta );
 
+});
+
+
+self.addEventListener( 'fetch', e => {
+
+    const respuesta = caches.match( e.request ).then( res => {
+
+        if ( res ) {
+            return res;
+        } else {
+
+            return fetch( e.request ).then( newRes => {
+
+                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
+
+            });
+        }
+
+       // console.log(res);
+
+    });
+
+    e.respondWith( respuesta );
 });
